f3cf1ab2742d271677c05e866bc4fbaf031959e5
xnaut97 <69466763+xnaut97@users.noreply.github.com>
2026-05-15 16:41:16 +0700

    Update Libraries.java

--- cosmos-core/src/main/java/com/github/xnaut97/cosmos/library/Libraries.java
+++ cosmos-core/src/main/java/com/github/xnaut97/cosmos/library/Libraries.java
@@ -34,7 +34,42 @@ public enum Libraries implements LibraryObject {
             "13.6.0",
             "xseries_library",
             null
-    )
+    ),
+    COSMOS_CORE(
+            "com.github.xnaut97",
+            "cosmos-core",
+            "1.0.0",
+            "cosmos_core_library",
+            null
+    ),
+    COSMOS_MENU(
+            "com.github.xnaut97",
+            "cosmos-menu",
+            "1.0.0",
+            "cosmos_menu_library",
+            null
+    ),
+    COSMOS_COMMAND(
+            "com.github.xnaut97",
+            "cosmos-command",
+            "1.0.0",
+            "cosmos_command_library",
+            null
+    ),
+    COSMOS_INPUT(
+            "com.github.xnaut97",
+            "cosmos-input",
+            "1.0.0",
+            "cosmos_input_library",
+            null
+    ),
+    COSMOS_UTILITIES(
+            "com.github.xnaut97",
+            "cosmos-utilities",
+            "1.0.0",
+            "cosmos_utilities_library",
+            null
+    ),
 
     ;
 
