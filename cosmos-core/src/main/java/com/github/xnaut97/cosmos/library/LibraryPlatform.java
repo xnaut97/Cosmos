301ec7b1154f6525d058ddcb0f78e1bcfdf347a9
xnaut97 <69466763+xnaut97@users.noreply.github.com>
2026-05-25 22:42:33 +0700

    Implement new module
    - Added module `cosmos-libraries`

--- cosmos-core/src/main/java/com/github/xnaut97/cosmos/library/LibraryPlatform.java
+++
@@ -1,6 +0,0 @@
-package com.github.xnaut97.cosmos.library;
-
-public enum LibraryPlatform {
-    BUKKIT,
-    PAPER,
-}
--- cosmos-libraries/src/main/java/com/github/xnaut97/cosmos/library/Libraries.java
+++ cosmos-libraries/src/main/java/com/github/xnaut97/cosmos/library/Libraries.java
@@ -99,3 +99,4 @@ public enum Libraries implements LibraryObject {
     }
 
 }
+
